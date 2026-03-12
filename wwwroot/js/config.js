a4b3c816b3795c91a2dbb16d5d43cc8648d43489
jef <jef@smartenergycontrol.be>
2026-03-12 14:56:06 +0100

    Add language compatibility

--- wwwroot/js/config.js
+++ wwwroot/js/config.js
@@ -24,45 +24,45 @@ export const TARGET_HEIGHT = 2.5;
 export const DEV = false;
 
 export const AVAILABLE_HUNTERS = [
-    { name: "Hell Ember", folder: "Hell_Ember" },
-    { name: "Ripper", folder: "Ripper" },
-    { name: "Gamekeeper", folder: "Gamekeeper" },
-    { name: "Soul Weaver", folder: "Soul_Weaver" },
-    { name: "Geisha", folder: "Geisha" },
-    { name: "Feaster", folder: "Feaster" },
-    { name: "Wu Chang", folder: "Wu_Chang" },
-    { name: "Photographer", folder: "Photographer" },
-    { name: "Mad Eyes", folder: "Mad_Eyes" },
-    { name: "Dream Witch", folder: "Dream_Witch" }
+    { name: "Hell Ember", folder: "厂长" },
+    { name: "Ripper", folder: "杰克" },
+    { name: "Gamekeeper", folder: "鹿头" },
+    { name: "Soul Weaver", folder: "蜘蛛" },
+    { name: "Geisha", folder: "红蝶" },
+    { name: "Feaster", folder: "黄衣之主" },
+    { name: "Wu Chang", folder: "白无常" },
+    { name: "Photographer", folder: "摄影师" },
+    { name: "Mad Eyes", folder: "疯眼" },
+    { name: "Dream Witch", folder: "梦之女巫" }
 ];
 
 export const AVAILABLE_SURVIVORS = [
-    { name: "Doctor", folder: "Doctor" },
-    { name: "Lawyer", folder: "Lawyer" },
-    { name: "Thief", folder: "Thief" },
-    { name: "Gardener", folder: "Gardener" },
-    { name: "Mechanic", folder: "Mechanic" },
-    { name: "Coordinator", folder: "Coordinator" },
-    { name: "Mercenary", folder: "Mercenary" },
-    { name: "Forward", folder: "Forward" },
-    { name: "Priestess", folder: "Priestess" },
-    { name: "Perfumer", folder: "Perfumer" },
-    { name: "Seer", folder: "Seer" },
-    { name: "Magician", folder: "Magician" },
-    { name: "Little Girl", folder: "Little_Girl"}
+    { name: "Doctor", folder: "医生" },
+    { name: "Lawyer", folder: "律师" },
+    { name: "Thief", folder: "慈善家" },
+    { name: "Gardener", folder: "园丁" },
+    { name: "Mechanic", folder: "机械师" },
+    { name: "Coordinator", folder: "空军" },
+    { name: "Mercenary", folder: "佣兵" },
+    { name: "Forward", folder: "前锋" },
+    { name: "Priestess", folder: "祭司" },
+    { name: "Perfumer", folder: "调香师" },
+    { name: "Seer", folder: "先知" },
+    { name: "Magician", folder: "魔术师" },
+    { name: "Little Girl", folder: "小女孩"}
 ];
 
 export const DEV_DATA = {
     hunter: {
         name: "Hell Ember",
         hasModel: true,
-        modelPath: "./hunters/Hell_Ember/",
-        modelFile: "Hell_Ember.gltf"
+        modelPath: "./hunters/厂长/",
+        modelFile: "厂长.gltf"
     },
     survivors: [
-        { name: "Doctor", hasModel: true, modelPath: "./survivors/Doctor/", modelFile: "Doctor.gltf" },
-        { name: "Night Watch", hasModel: true, modelPath: "./hunters/Night_Watch/", modelFile: "Night_Watch.gltf" },
-        { name: "Thief", hasModel: true, modelPath: "./survivors/Thief/", modelFile: "Thief.gltf" },
-        { name: "Gardener", hasModel: true, modelPath: "./survivors/Gardener/", modelFile: "Gardener.gltf" }
+        { name: "Doctor", hasModel: true, modelPath: "./survivors/医生/", modelFile: "医生.gltf" },
+        { name: "Night Watch", hasModel: true, modelPath: "./hunters/芬妮·吉尔曼/", modelFile: "芬妮·吉尔曼.gltf" },
+        { name: "Thief", hasModel: true, modelPath: "./survivors/慈善家/", modelFile: "慈善家.gltf" },
+        { name: "Gardener", hasModel: true, modelPath: "./survivors/园丁/", modelFile: "园丁.gltf" }
     ]
 };
